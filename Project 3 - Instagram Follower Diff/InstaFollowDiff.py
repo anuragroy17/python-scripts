c74a3bf1bf9f496c5a43c732f77824643fee9734
anuragroy17 <anuragroy17@gmail.com>
2020-06-12 19:15:08 +0530

    final changes

--- Project 3 - Instagram Follower Diff/InstaFollowDiff.py
+++ Project 3 - Instagram Follower Diff/InstaFollowDiff.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep, strftime
 import getpass
+from random import randrange
+import csv
 
 
 class InstaFollowDiff:
@@ -13,10 +16,6 @@ class InstaFollowDiff:
         bot = self.bot
         bot.get("https://www.instagram.com/")
         sleep(2)
-
-        # loginBtn = bot.find_element_by_xpath("//a[contains(text(), 'Log in')]")
-        # loginBtn.click()
-        # sleep(1)
 
         emailInput = bot.find_elements_by_css_selector('form input')[0]
         passwordInput = bot.find_elements_by_css_selector('form input')[1]
@@ -37,10 +36,61 @@ class InstaFollowDiff:
             "//button[contains(text(), 'Not Now')]")
         notnow.click()
 
-    def logout(self):
+    def openUser(self):
         bot = self.bot
 
-        if bot.current_url != f'https://www.instagram.com/{self.username}':
+        profile = bot.find_element_by_xpath(
+            "//a[contains(@href,'/{}')]".format(self.username))
+        profile.click()
+        sleep(2)
+
+    def get_unfollowers(self):
+        bot = self.bot
+
+        following_button = bot.find_element_by_xpath(
+            "//a[contains(@href,'/following')]/span")
+        following_button.click()
+        following = self._get_names(int(following_button.text))
+        followers_button = bot.find_element_by_xpath(
+            "//a[contains(@href,'/followers')]/span")
+        followers_button.click()
+        followers = self._get_names(int(followers_button.text))
+        not_following_back = [
+            user for user in following if user not in followers]
+
+        with open('unfollowers.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Username", "Url"])
+            for x in not_following_back:
+                writer.writerow([x, f'https://www.instagram.com/{x}/'])
+
+    def _get_names(self, count):
+        sleep(3)
+        bot = self.bot
+
+        list = bot.find_element_by_xpath("//div[@role='dialog']")
+        list.click()
+
+        links = list.find_elements_by_xpath("//li")
+        loaded_names_count = len(links)
+
+        actionChain = webdriver.ActionChains(bot)
+        while (loaded_names_count < count):
+            actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+            sleep(randrange(2) + 1)
+            links = list.find_elements_by_xpath("//li")
+            loaded_names_count = len(links)
+
+        nameList = list.find_elements_by_tag_name('a')
+        names = [name.text for name in nameList if name.text != '']
+        # close button
+        closeBtn = bot.find_element_by_css_selector('[aria-label=Close]')
+        closeBtn.click()
+        return names
+
+    def logout(self):
+        bot = self.bot
+        if bot.current_url != f'https://www.instagram.com/{self.username}/':
             profile = bot.find_element_by_xpath(
                 "//a[contains(@href,'/{}')]".format(self.username))
             profile.click()
@@ -53,12 +103,19 @@ class InstaFollowDiff:
         logOut = bot.find_element_by_xpath(
             "//button[contains(text(), 'Log Out')]")
         logOut.click()
+        sleep(2)
 
     def closeBrowser(self):
         self.bot.close()
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.closeBrowser()
+
+
+class unfollowers:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
 
 
 try:
@@ -68,6 +125,8 @@ try:
     print("Automating your insta life...wait!!")
     botCrawl = InstaFollowDiff(username, password)
     botCrawl.login()
+    botCrawl.openUser()
+    botCrawl.get_unfollowers()
 
     botCrawl.logout()
     botCrawl.closeBrowser()
