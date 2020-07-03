0e27e9ad09d7fe8553528fd3a8e276f84038162e
anuragroy17 <anuragroy17@gmail.com>
2020-07-04 03:44:30 +0530

    windows battery notifier added

---
+++ Project 4 - Windows Battery Notifier/BatteryNotifier.py
@@ -0,0 +1,66 @@
+import psutil
+import time
+import pyttsx3
+from win10toast import ToastNotifier
+import threading
+
+# save file with extension .pyw and save it with the icon in windows startup folder
+# for win10toast to show up, press win+i>System>Notification & Actions>Switch on "Get Notications from apps and others"
+
+toaster = ToastNotifier()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)  # 1 for female, 0 for male
+engine.setProperty('rate', 120)
+engine.setProperty('volume', 8)
+count = 0
+
+
+def showToastrNotifications(text):
+    toaster.show_toast(text, icon_path='battery_icon.ico', duration=10) # For same folder
+    # toaster.show_toast(text, icon_path="C://battery_icon.ico', duration=10) # Full path
+	
+    # looping the toaster for some period of time
+    while toaster.notification_active():
+        time.sleep(0.1)
+
+
+def monitorPercentage():
+    global count
+    while (True):
+        time.sleep(10)
+        battery = psutil.sensors_battery()
+        plugged = battery.power_plugged
+        percent = int(battery.percent)
+
+        if percent < 15:
+            if plugged == False:
+                processThread = threading.Thread(target=showToastrNotifications, args=(
+                    f"Battery is at {percent}% Please plug the cable",))  # <- note extra ','
+                processThread.start()
+                engine.say(
+                    "Your battery is getting low so plug in your charger")
+                engine.runAndWait()
+                count = 0
+        elif percent == 100:
+            if plugged == True:
+                processThread = threading.Thread(target=showToastrNotifications, args=(
+                    "Battery is Fully Charged",))
+                processThread.start()
+                engine.say(
+                    "Battery is fully charged, please unplug the charger")
+                engine.runAndWait()
+        elif percent == 90:
+            if plugged == True:
+                if count == 0:
+                    processThread = threading.Thread(target=showToastrNotifications, args=(
+                        "Your Battery at 90%. You can unplug the charger.",))
+                    processThread.start()
+                    engine.say(
+                        "Your battery is at 90%, you can unplug the charger")
+                    engine.runAndWait()
+                    count = count + 1
+
+
+if __name__ == "__main__":
+    monitorPercentage()
